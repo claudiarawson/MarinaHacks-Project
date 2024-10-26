e735625e057d23e3241e398ad6e9c07c2ea83421
claudiarawson <claudiarawson2021@gmail.com>
2024-10-26 16:29:47 -0700

    Header Components

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ const App = () => {
         // implementation
     }
   return (
-    <div class="header">
-      <header-button type="header-button" onClick={handleClick}>
-        Home
-      </header-button>
-      <header-button type="header-button" onClick={handleClick}>
-        Grades
-      </header-button>
-      <header-button type="header-button" onClick={handleClick}>
-        Schedule
-      </header-button>
-      <header-button type="header-button" onClick={handleClick}>
-        Community
-      </header-button>
-      <header-button type="header-button" onClick={handleClick}>
-        Next Two Weeks
-      </header-button>
+    <div>
+      <div class="header">
+        <header-button type="header-button" onClick={handleClick}>
+          Home
+        </header-button>
+        <input type="text" placeholder="Search..."/>
+        <header-button type="header-button" onClick={handleClick}>
+          Grades
+        </header-button>
+        <header-button type="header-button" onClick={handleClick}>
+          Schedule
+        </header-button>
+        <header-button type="header-button" onClick={handleClick}>
+          Community
+        </header-button>
+        <header-button type="header-button" onClick={handleClick}>
+          Next Two Weeks
+        </header-button>
+      </div>
     </div>
+
   );
 };
 
