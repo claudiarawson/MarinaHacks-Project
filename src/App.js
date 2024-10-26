fe38aecceacb8fbce2d29cf6e5f916f12744b69f
claudiarawson <claudiarawson2021@gmail.com>
2024-10-26 12:53:12 -0700

    More Buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+const App = () => {
+    const [count, setCount] = React.useState(0);
+    const handleClick = () => {
+        // implementation
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={handleClick}>
+        Home
+      </button>
+      <button type="button" onClick={handleClick}>
+        Grades
+      </button>
+      <button type="button" onClick={handleClick}>
+        Schedule
+      </button>
+      <button type="button" onClick={handleClick}>
+        Community
+      </button>
     </div>
   );
-}
+};
 
 export default App;
