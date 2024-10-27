01d60b3a681aaca333009303c1ee9e81438facba
Jason Gonzalez <jason.gonzalez0112@gmail.com>
2024-10-27 09:48:53 -0700

    Courses Page created + dashboard reroute started

--- src/App.js
+++ src/App.js
@@ -16,14 +16,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Components/dashboard';
 import Calendar from './Components/calendar';
+import Navbar from './Components/navbar';
+import CourseNav from './Components/courseNav';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {<Navbar />}
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/calendar" element={<Calendar />} />
+          <Route path="/courseNav" element={<CourseNav />} />
         </Routes>
       </div>
     </Router>
--- src/Components/calendar.js
+++ src/Components/calendar.js
@@ -1,5 +1,6 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import dashboard from './dashboard';
 
 
 export default function Example(){
