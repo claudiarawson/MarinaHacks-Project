3b7af74f1f8d862d3d7664cf4486dae914b9bbb0
Jason Gonzalez <jason.gonzalez0112@gmail.com>
2024-10-27 05:22:38 -0700

    Calendar implementation - button works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Dashboard from './Components/dashboard';
-import Dropdown from './Components/dropdown';
+/*import Dashboard from './Components/dashboard';
+import Calendar from './Components/calendar';
 
 function App() {
   return (
@@ -11,3 +11,23 @@ function App() {
 
 export default App;
 // notesgit
+*/
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Components/dashboard';
+import Calendar from './Components/calendar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
